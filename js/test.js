b4751a1f38435b0285d6e4964f207c6d38c5023f
Masahiko Nakatomi <masahiko_nakatomi@MasahikonoMacBook-Pro.local>
2021-02-05 14:57:12 +0900

    hotfix add test code

--- js/test.js
+++ js/test.js
@@ -1,15 +1,17 @@
-$(() => {
-	var queryParam = window.location.search.substr(1).split("=");
-	console.log("key : " + queryParam[0]);
-	console.log("value : " + queryParam[1]);
-	var loginHeader = $("#loginHeader").text();
-	console.log("loginHeader : " + loginHeader);
+var queryParam = window.location.search.substr(1).split("=");
+console.log("key : " + queryParam[0]);
+console.log("value : " + queryParam[1]);
+var loginHeader = $("#loginHeader").text();
+console.log("loginHeader : " + loginHeader);
 
-	if (loginHeader == "1") {
-		kzs("setCustomVariable", "isLogin", loginHeader);
-	}
+if (loginHeader == "1") {
+	kzs("setCustomVariable", "isLogin", loginHeader);
+}
 
-	if (window.location.href.match("/complete")) {
-		kzs("trackClickGoals", "35258");
-	}
-});
+if (window.location.href.match("/complete")) {
+	kzs("trackClickGoals", "35258");
+}
+
+if (window.location.href.match("/confirm.html")) {
+	kzs("setCustomVariable", "isLogin", loginHeader);
+}
