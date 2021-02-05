a500acf6b6712a457c5d1202b1dff967364788df
Masahiko Nakatomi <masahiko_nakatomi@MasahikonoMacBook-Pro.local>
2021-02-05 15:51:49 +0900

    hotfix add test code

--- js/test.js
+++ js/test.js
@@ -3,6 +3,7 @@ console.log("key : " + queryParam[0]);
 console.log("value : " + queryParam[1]);
 var loginHeader = $("#loginHeader").text();
 console.log("loginHeader : " + loginHeader);
+console.log("deploy3");
 
 if (loginHeader == "1") {
 	kzs("setCustomVariable", "isLogin", loginHeader);
@@ -13,5 +14,30 @@ if (window.location.href.match("/complete")) {
 }
 
 if (window.location.href.match("/confirm.html")) {
-	kzs("setCustomVariable", "isLogin", loginHeader);
+	console.log("productName1" + $("#productName1").text());
+	console.log("productNum1" + $("#productNum1").text());
+	console.log("money1" + $("#money1").text());
+	console.log("productName2" + $("#productName2").text());
+	console.log("productNum2" + $("#productNum2").text());
+	console.log("money2" + $("#money2").text());
+	document.cookie = "productName1=" + $("#productName1").text();
+	document.cookie = "productNum1=" + $("#productNum1").text();
+	document.cookie = "money1=" + $("#money1").text();
+	document.cookie = "productName2=" + $("#productName2").text();
+	document.cookie = "productNum2=" + $("#productNum2").text();
+	document.cookie = "money2=" + $("#money2").text();
+}
+
+if (window.location.href.match("/buyComp.html")) {
+	console.log(document.cookie);
+	const cookieValue = document.cookie.split("; ");
+	const find = (str) =>
+		cookieValue.find((row) => row.startsWith(str)).split("=")[1];
+	const productNum1 = find("productNum1");
+	const money1 = find("money1");
+	const productNum2 = find("productNum2");
+	const money2 = find("money2");
+	const total = productNum1 * money1 + productNum2 * money2;
+	console.log("total = " + total);
+	document.cookie = "total_amount=" + total;
 }
