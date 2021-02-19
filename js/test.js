322d6f458514dee6ff006e5dc17319ed3ae12e19
Masahiko Nakatomi <masahiko_nakatomi@MasahikonoMacBook-Pro.local>
2021-02-19 17:32:28 +0900

    hotfix add test code

--- js/test.js
+++ js/test.js
@@ -1,8 +1,8 @@
 "use strict";
 // exp script template (Promise が不要な施策用)
 // basic-with-promise.js と特徴は同じ
-var _ver = "20191115-1"; // ①code 更新毎に手動で値を変更すべし
-var _pid = "trackUserSegment"; // ②ここを施策内容を表すラベルに変更
+var _ver = "20210219-1"; // ①code 更新毎に手動で値を変更すべし
+var _pid = "testTrackingGoalSetting"; // ②ここを施策内容を表すラベルに変更
 var _exp = this;
 var _expId = (((_exp.variation || {}).round || {}).project || {}).id;
 var _el = "exp#" + (_expId || "none"); // console log 用 prefix label
@@ -19,34 +19,34 @@ var _el = "exp#" + (_expId || "none"); // console log 用 prefix label
 			kzs[_expId] = 1;
 		}
 
-		// editor or preview mode では何もしない: 施策要件次第では要削除
-		if (kzs.utils.isNotInEditorOrPreviewMode() == false) {
-			kzs.console.log("%s in editor||preview mode, do nothing", _el);
-			return;
-		}
+		// // editor or preview mode では何もしない: 施策要件次第では要削除
+		// if (kzs.utils.isNotInEditorOrPreviewMode() == false) {
+		// 	kzs.console.log("%s in editor||preview mode, do nothing", _el);
+		// 	return;
+		// }
 
-		// オリジナル案ならば何もしない: 施策要件次第では要削除
-		if ((_exp.variation || {}).isOriginal == true) {
-			kzs.console.log("%s is original, do nothing", _el);
-			return;
-		}
+		// // オリジナル案ならば何もしない: 施策要件次第では要削除
+		// if ((_exp.variation || {}).isOriginal == true) {
+		// 	kzs.console.log("%s is original, do nothing", _el);
+		// 	return;
+		// }
 
 		// ここから施策 code 実行
 		$(function () {
-			$(".h1").attr("data-kz-track", "outbound");
+			kzs.console.log("施策を実施");
 			$(".link-list.type-local-navi li:nth-child(1) a").attr(
 				"data-kz-track-on-click",
-				"35275"
+				"35426"
 			);
 
 			$(".link-list.type-local-navi li:nth-child(2) a").attr(
 				"data-kz-track-on-click",
-				"35275"
+				"35427"
 			);
 
 			$(".link-list.type-local-navi li:nth-child(3) a").attr(
 				"data-kz-track-on-click",
-				"35275"
+				"35428"
 			);
 		});
 
